ad36dc95c722876d6f1f37b284af9b15a2066615
Jaxer6563 - Desktop <130395049+Jaxer6563@users.noreply.github.com>
2023-04-10 14:29:47 -0400

    Update typing_test.py

--- typing_test.py
+++ typing_test.py
@@ -4,13 +4,13 @@ def typingPrint(text):
     for character in text:
         sys.stdout.write(character)
         sys.stdout.flush()
-        time.sleep(0.05)
+        time.sleep(0.1)
 
 def typingInput(text):
     for character in text:
         sys.stdout.write(character)
         sys.stdout.flush()
-        time.sleep(0.05)
+        time.sleep(0.1)
     value=input()
     return value
 
