3ec94350558b99899c07e18942cb63184271c81c
Jaxer6563 - Laptop <130395049+Jaxer6563@users.noreply.github.com>
2023-04-10 16:26:19 -0400

    Update typing_test.py

--- typing_test.py
+++ typing_test.py
@@ -1,23 +1,25 @@
-import time, sys
+import time, sys, colorama
+from colorama import Fore
 
 def typingPrint(text):
     for character in text:
         sys.stdout.write(character)
         sys.stdout.flush()
-        time.sleep(0.1)
+        time.sleep(0.05)
 
 def typingInput(text):
     for character in text:
         sys.stdout.write(character)
         sys.stdout.flush()
-        time.sleep(0.1)
+        time.sleep(0.05)
     value=input()
     return value
 
-###
-#typingPrint("Hello Stupid Humans \nYou are a stupid Human\n")
-#typingInput("How do you feel about being a stupid Human?\n")
-#typingPrint("FeelsBadMan")
 
-###
-typingPrint("You take the blue pill... the story ends, you wake up in your bed and beleive whatever you want to believe.")
+typingPrint("Hello Stupid Humans \nYou are a stupid Human\n")
+typingInput("How do you feel about being a stupid Human?\n")
+typingPrint("FeelsBadMan")
+
+
+#typingPrint("You take the blue pill... the story ends, you wake up in your bed and believe whatever you want to believe. \nYou take the red pill... You stay in Wonderland, and I show you how deep the rabbit hole goes.")
+
