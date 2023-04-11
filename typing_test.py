55e4998deadccc2394a1aacea85231ef2d5928b3
Jaxer6563 - Laptop <130395049+Jaxer6563@users.noreply.github.com>
2023-04-10 20:47:04 -0400

    Update typing_test.py

--- typing_test.py
+++ typing_test.py
@@ -1,15 +1,15 @@
 import time, sys, colorama
-from colorama import Fore
-
+from colorama import Fore, init, Back
+init()
 def typingPrint(text):
     for character in text:
-        sys.stdout.write(character)
+        sys.stdout.write(Fore.BLACK + Back.GREEN + character)
         sys.stdout.flush()
         time.sleep(0.05)
 
 def typingInput(text):
     for character in text:
-        sys.stdout.write(character)
+        sys.stdout.write(Fore.BLACK + Back.GREEN + character)
         sys.stdout.flush()
         time.sleep(0.05)
     value=input()
