96b95fd270297711f532b4b1e9f9e45c42b0d0d7
Jaxer6563 - Desktop <130395049+Jaxer6563@users.noreply.github.com>
2023-04-11 14:24:20 -0400

    Update typing_test.py

--- typing_test.py
+++ typing_test.py
@@ -15,11 +15,24 @@ def typingInput(text):
     value=input()
     return value
 
-esc = chr(27)
-typingPrint("Hello Stupid Humans \nYou are a stupid Human\n")
-typingInput("How do you feel about being a stupid Human?\n")
-typingPrint("FeelsBadMan")
-#print(f'a{}[5m_\u2592b\u2588{esc}[m_c')
 
-#typingPrint("You take the blue pill... the story ends, you wake up in your bed and believe whatever you want to believe. \nYou take the red pill... You stay in Wonderland, and I show you how deep the rabbit hole goes.")
+#typingPrint("Hello Stupid Humans \nYou are a stupid Human\n")
+#typingInput("How do you feel about being a stupid Human?\n")
+#typingPrint("FeelsBadMan")
 
+
+typingPrint("You take the blue pill... the story ends, you wake up in your bed and believe whatever you want to believe. \nYou take the red pill... You stay in Wonderland, and I show you how deep the rabbit hole goes.\n")
+x = typingInput("You can only take one. Choose Now\n")
+if x == "Red":
+    typingPrint("You are trapped in the Matrix forever")
+    input()
+elif x == "Blue":
+    typingPrint("You have escaped the Matrix, and wake up in your bed")
+    input()
+elif x == "Both":
+    typingPrint("You fucking Moron.\n")
+    time.sleep(2)
+    typingPrint("You are dead...")
+    input()
+else:
+    exit()
