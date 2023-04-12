21e9bda6eeb45d28973eb49345793ef62146c04d
Jaxer6563 - Laptop <130395049+Jaxer6563@users.noreply.github.com>
2023-04-12 08:25:18 -0400

    Changed Clear.py and Main_Terminal.py

--- Lib/clear/clear.py
+++ Lib/clear/clear.py
@@ -1,10 +1,7 @@
-# Import only system and name from os
+# Module that allows for the program to determine what opperating system the user is using, and then clears the screen when called
 from os import system, name
 
-# Import sleep to show output for some time period
-from time import sleep
 
-# Define our clear function
 def clear():
     if name == 'nt' :   # For Windows
         _ = system('cls')
--- Main_Terminal.py
+++ Main_Terminal.py
@@ -14,6 +14,23 @@ def startup():
     print("WELCOME TO ROBCO INDUSTRIES (TM) TERMLINK")
     time.sleep(1)
     typing.typingPrintDelay("\n ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓",0.5)
+    time.sleep(1)
+    clear.clear()
+    
+    
+def home():
+    print("WELCOME TO ROBCO INDUSTRIES (TM) TERMLINK\n")
+    time.sleep(1)
+    typing.typingPrint(FileName1)
+    time.sleep(1)
+    typing.typingPrint(FileName2 )
+    time.sleep(1)
+    input()
+
 typing.typingPrint("WELCOME TO ROBCO INDUSTRIES (TM) TERMLINK")
+FileName1 = "Tips\n\n"
+FileName2 = "Chem Supplier Notes\n\n"
+
 time.sleep(1)
 startup()
+home()
